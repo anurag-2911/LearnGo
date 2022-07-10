6c6e6bfd5d985c0ef44cc36c39c1c2da3462c99e
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-07-10 08:59:19 +0530

    receiver function.

--- cards/main.go
+++ cards/main.go
@@ -5,21 +5,29 @@ import "fmt"
 func main() {
 	var card string = "first card" // variable declaration
 	fmt.Println(card);
+
 	name:="anurag" // variable declaration
 	fmt.Println(name);
+
 	var nm string = getName();
 	fmt.Println(nm);
+
 	
 	// array | slice
 
-	cards:= []string{"card1","card2"}
+	cards:= deck{"card1","card2","card3"}
 
-	fmt.Println(cards)
+	// fmt.Println(cards)
 
 	cards = append(cards, "card3")
 
-	fmt.Println(cards)
+	// fmt.Println(cards)
+    // for loop
+	// for i, v := range cards {
+	// 	fmt.Println(i,v);
+	// }
 
+	cards.print()
 }
 
 func getName() string {
