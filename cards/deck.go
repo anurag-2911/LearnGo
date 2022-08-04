e9b665c4083a4a60a87fb0d990dfe54dbbc9e07f
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-08-04 23:06:20 +0530

    clean-up of files.

--- cards/deck.go
+++ cards/deck.go
@@ -2,12 +2,15 @@ package main
 
 import "fmt"
 
+// create a new type of desk
+// which has slice of strings
+
 type deck []string
 
 //receiver function
 func (cards deck) print() {
 	for i, card := range cards {
-		fmt.Println(i,card)
+		fmt.Println(i, card)
 
 	}
 
--- cards/main.go
+++ cards/main.go
@@ -1,24 +1,41 @@
 package main
 
-import 
-(
+import (
 	"fmt"
 	"strconv"
 )
 
 func main() {
+	callBasics()
+}
+
+func callBasics() {
 	fmt.Print(" hello world ")
 	var card string = " variable declaration "
 	fmt.Print(card)
 	var1 := " another name "
 	var1 = " yes "
 	fmt.Print(var1)
-	fmt.Print(names() + " " + strconv.Itoa(age()))
-	
+	fmt.Print(name() + " " + strconv.Itoa(age()))
+	var names []string = append(getNames(), "khalkho")
+	fmt.Print(names)
+	printNames(names)
 }
-func names() string {
-	return " anurag ";
+func name() string {
+	return " anurag "
 }
 func age() int {
-	return 1;
+	return 1
+}
+
+func getNames() []string {
+	var nms = []string{"topno", "kunar", "benjamin"}
+	return nms
+}
+
+func printNames(names []string) {
+	for i, name := range names {
+		fmt.Print(i, name)
+		fmt.Print(" ")
+	}
 }
