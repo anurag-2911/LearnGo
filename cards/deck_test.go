b279d7f00aa64e489a430748e06f8e837edb44f5
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-08-06 23:49:13 +0530

    one test case example.

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -6,6 +6,12 @@ import (
 )
 
 func TestNewDeck(t *testing.T) {
-fmt.Println("testing new deck feature")
+	fmt.Println("testing new deck feature")
+	newCard := newDeck()
+
+	var length int = len(newCard)
+	if length != 16 {
+		t.Error("length is not 16 but ", length)
+	}
 
 }
