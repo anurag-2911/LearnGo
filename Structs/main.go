612d946699dd416bd2ba2cdcfeef196cca8e9746
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-08-11 23:26:20 +0530

    about pointers

--- Structs/main.go
+++ Structs/main.go
@@ -25,8 +25,8 @@ func main() {
 	p1 = person{firstName: "michael", lastName: "topno"}
 	fmt.Println(p1)
 
-	p4:=person{firstName: "ram",lastName: "naam"}
-	fmt.Println(p4);
+	// p4:=person{firstName: "ram",lastName: "naam"}
+	// fmt.Println(p4);
 	var p2 person
 	p2.firstName = "james"
 	p2.lastName = "murmu"
@@ -42,11 +42,33 @@ func main() {
 	p3.print()
 
 	pointp3:= &p3
+	fmt.Println("memory address ")
+	fmt.Println(*pointp3)
 	pointp3.updateNamebyPointer("vinod")
 	pointp3.print()
 
+	var p4 person
+	p4.firstName="jklf"
+	p4.lastName= "hghg"
+	c1 :=contact{email: "g",pincode: "11"}
+	p4.cinfo = c1
+
+	p4.updateNamebyPointer("kunar")
+	p4.print()
+
+	myslice := []string {"hello","hi","sukkhi"}
+	updateSlice(myslice)
+	fmt.Println(myslice)
+    name:="kachchap"
+	fmt.Println(*(&name))
+	fmt.Scanf("h") // wait for the input
 
 }
+
+func updateSlice(slice []string){
+slice[0]="bye"
+}
+
 func(p *person)updateNamebyPointer(newFirstname string){
 	(*p).firstName=newFirstname
 }
