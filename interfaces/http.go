75dd72f6f589a455e2ca829b8c694bc218480976
Anurag Kumar <kodelabzz.dev@gmail.com>
2022-08-13 21:30:40 +0530

    getting http response

---
+++ interfaces/http.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+type logWriter struct{}
+func main() {
+	resp, err := http.Get("http://www.google.com")
+
+	if(err!=nil){
+		fmt.Println("error happened " ,err)
+		os.Exit(1)
+	}
+	var lw logWriter
+    io.Copy(lw,resp.Body)
+
+
+}
+
+func (lw logWriter)Write(data []byte)(int,error){
+	fmt.Println(string(data))
+	return len(data),nil
+}
